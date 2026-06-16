44ee9e2ee303d3248e7f3dfe518d0154c64b8fb2
Sophiel Zhou <sophiel@zzy040330.moe>
2026-06-16 23:37:29 +0800

    fix: js licenses ahh

---
+++ static/js/katex-init.js
@@ -0,0 +1,11 @@
+// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
+document.addEventListener('DOMContentLoaded', function() {
+  renderMathInElement(document.body, {
+    delimiters: [
+      {left: '$$',  right: '$$',  display: true},
+      {left: '\\[', right: '\\]', display: true},
+      {left: '\\(', right: '\\)', display: false}
+    ]
+  });
+});
+// @license-end
